bd15698f6d0cb08b98bc904ed670a92b15e7e1cd
sriya seethepalli <sriya.s@seanergy.ai>
2025-12-10 14:10:53 +0530

    feat(main) : Add the flow in the main and create a run_timesheet_sync function

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from timesheet!")
+from toggle import fetch_toggl_tasks, create_response_body_from_toggl_taks
+from update_api import push_to_timesheet_api
+from popup import show_edit_popup
+from config import settings
+
+
+def run_timesheet_sync():
+    raw_entries = fetch_toggl_tasks(settings.toggle_api)
+    formatted_entries = create_response_body_from_toggl_taks(raw_entries)
+
+    edited_entries = show_edit_popup(formatted_entries)
+
+    if not edited_entries:
+        print("Cancelled. No API calls made.")
+        return
+
+    for entry in edited_entries:
+        status, msg = push_to_timesheet_api(entry)
+        print(f"[{entry.date}] → {entry.task} → {status} → {msg}")
 
 
 if __name__ == "__main__":
-    main()
+    run_timesheet_sync()
