4913808a6fbb67218f8b1c897ba2bd1640e5f6ce
sriya seethepalli <sriya.s@seanergy.ai>
2025-12-10 14:09:45 +0530

    Refactor(config) : Update the config with the updated env variables

--- config.py
+++ config.py
@@ -2,11 +2,19 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+
 class Settings:
-    claude_api_key = os.getenv("ANTHROPIC_API_KEY")
+    google_api_key = os.getenv("google_api_key")
     toggle_url = os.getenv("toggle_url")
     toggle_api = os.getenv("toggle_api_key")
     time_sheet_url = os.getenv("time_sheet_url")
+    time_zone = os.getenv("time_zone")
+    google_model = os.getenv("google_model")
+    emp_id = os.getenv("emp_id")
+    project_id = os.getenv("project_id")
+
+
 try:
     settings = Settings()
 except Exception as e:
