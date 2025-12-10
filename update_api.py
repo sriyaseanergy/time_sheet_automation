496c9926917a033a6709dd0b68ccb1d4c73b8b35
sriya seethepalli <sriya.s@seanergy.ai>
2025-12-10 14:13:53 +0530

    feat(update_api) : create the update api to push the entries to time sheet

---
+++ update_api.py
@@ -0,0 +1,22 @@
+import requests
+from config import settings
+
+
+def push_to_timesheet_api(entry):
+    """
+    entry = Apiparameters dataclass instance
+    """
+
+    payload = {
+        "empid": entry.empid,
+        "date": entry.date,
+        "tasktype": entry.tasktype,
+        "functionality": entry.functionality,
+        "task": entry.task,
+        "timespent": entry.timespent,
+        "projectUID": entry.projectUID,
+    }
+
+    response = requests.post(settings.time_sheet_url, json=payload, timeout=10)
+
+    return response.status_code, response.text
