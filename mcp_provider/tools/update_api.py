be95e97882a243954435545e7237ab26e9459369
sriya seethepalli <sriya.s@seanergy.ai>
2025-12-22 15:15:43 +0530

    refactor: remove mcp_provider module and its components

--- mcp_provider/tools/update_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-from config import settings
-
-def push_to_timesheet_api(task, updated_text):
-    r = requests.post(
-        settings.time_sheet_url,
-        json={"id": task["id"], "description": updated_text},
-    )
-    return r.status_code
